ef707478b2e70a06bd089897c50ca0c1aad1c935
jrivilla <jrivillarepository@gmail.com>
2022-11-29 16:33:56 +0100

    feat: added app components

--- curso_intro_react/src/App.js
+++ curso_intro_react/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import react from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButtom } from "./CreateTodoButton";
+
+
+const todos = [
+  { text: 'Cortar Cebolla', completed: false },
+  { text: 'Tomar el curso de React', completed: false },
+  { text: 'Lorar mucho', completed: false }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <react.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButtom />      
+    </react.Fragment>
+
   );
 }
 
---
+++ curso_intro_react/src/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoItem(props){
+    return(
+        <li>
+            <span></span>
+            <p>{props.text}</p>
+            <span></span>
+        </li>
+    );
+}
+
+export { TodoItem };
